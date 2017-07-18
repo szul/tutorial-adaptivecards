201cb63fcbd17a93b41f358410a7b7c48c66970a
Michael Szul <michael@szul.us>
2017-07-18 13:31:48 -0400

    Started TS items

--- static/ts/app.ts
+++ static/ts/app.ts
@@ -1,1 +1,5 @@
-/// <reference path="../../node_modules/microsoft-adaptivecards/dist/adaptive-cards.d.ts" />
+/// <reference path="../../node_modules/microsoft-adaptivecards/built/adaptive-cards.d.ts" />
+
+import * as AdaptiveCards from "../../node_modules/microsoft-adaptivecards/built/adaptive-cards";
+
+var card: AdaptiveCards.AdaptiveCard =  new AdaptiveCards.AdaptiveCard();
